f65936835b00c6e785f181cee4121cf39e9ccd66
SpongBob <2916863213@qq.com>
2018-10-17 16:50:25 +0800

    分页代码重构（region区域列表分页）

--- bos-web/src/main/java/com/imwj/bos/web/action/base/BaseAction.java
+++ bos-web/src/main/java/com/imwj/bos/web/action/base/BaseAction.java
@@ -1,12 +1,48 @@
 package com.imwj.bos.web.action.base;
 
+import java.io.IOException;
 import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
 
+import org.apache.struts2.ServletActionContext;
+import org.hibernate.criterion.DetachedCriteria;
+
+import com.imwj.bos.utils.PageQuery;
 import com.opensymphony.xwork2.ActionSupport;
 import com.opensymphony.xwork2.ModelDriven;
 
+import net.sf.json.JSONObject;
+import net.sf.json.JsonConfig;
+
 public class BaseAction<T> extends ActionSupport implements ModelDriven<T> {
+	
+	/**
+	 * 分页重构代码
+	 */
+	protected PageQuery pageQuery = new PageQuery();
+	//创建离线提交查询对象
+	DetachedCriteria detachedCriteria = null;
+	public void setPage(int page) {
+		pageQuery.setCurrentPage(page);
+	}
+	public void setRows(int rows) {
+		pageQuery.setPageSize(rows);
+	}
+	
+	public void javaToJson(Object o,String[] str){
+		//将pageQuery转换为json数据:
+		//JSONObject---将单一对象转为json  JSONArray----将数组或者集合对象转为json
+		JsonConfig jsonConfig = new JsonConfig();
+		jsonConfig.setExcludes(str);
+		String json = JSONObject.fromObject(o,jsonConfig).toString();
+		ServletActionContext.getResponse().setContentType("text/json;charset=utf-8");
+		try {
+			ServletActionContext.getResponse().getWriter().print(json);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
 	public static final java.lang.String HOME = "home";
 	public static final java.lang.String LIST = "list";
 	
@@ -24,6 +60,8 @@ public class BaseAction<T> extends ActionSupport implements ModelDriven<T> {
 		//获得BaseAction上声明的泛型数组
 		Type[] type = genericSuperclass.getActualTypeArguments();
 		Class<T> entityClass = (Class<T>) type[0];
+		detachedCriteria = DetachedCriteria.forClass(entityClass);
+		pageQuery.setDetachedCriteria(detachedCriteria);
 		
 		//通过反射创建对象
 		try {
