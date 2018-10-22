a528142ddb5dd2379361aa93f6ea727c9aebadac
SpongBob <2916863213@qq.com>
2018-10-22 16:55:11 +0800

    WebService的连接测试

--- bos-web/src/main/java/com/imwj/bos/web/action/UserAction.java
+++ bos-web/src/main/java/com/imwj/bos/web/action/UserAction.java
@@ -1,5 +1,7 @@
 package com.imwj.bos.web.action;
 
+import com.imwj.bos.crm.Customer;
+import com.imwj.bos.crm.ICustomerService;
 import com.imwj.bos.domain.User;
 import com.imwj.bos.service.IUserService;
 import com.imwj.bos.utils.BOSUtils;
@@ -7,6 +9,7 @@ import com.imwj.bos.web.action.base.BaseAction;
 import com.opensymphony.xwork2.ActionContext;
 
 import java.io.IOException;
+import java.util.List;
 
 import org.apache.commons.lang3.StringUtils;
 
@@ -21,6 +24,9 @@ import org.springframework.context.annotation.Scope;
 @Scope("prototype")
 public class UserAction extends BaseAction<User>  {
 
+	@Autowired
+	private ICustomerService porxy;
+	
 	//属性驱动，接收页面输入的验证码
 	private String checkcode;
 	public void setCheckcode(String checkcode) {
@@ -37,6 +43,10 @@ public class UserAction extends BaseAction<User>  {
 	 * @throws Exception
 	 */
 	public String login() throws Exception {
+//		WebService连接测试
+//		List<Customer> list = porxy.findAll();
+//		System.out.println(list);
+		
 		//得到session域中的验证码
 		String validatecode = (String) ServletActionContext.getRequest().getSession().getAttribute("key");
 		//先判断验证码是否输入正确
