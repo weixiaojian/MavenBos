a847d0234723cf02cc576960265cc85969f65566
SpongBob <2916863213@qq.com>
2018-10-25 17:55:59 +0800

    realm中进行授权

--- bos-service/src/main/java/com/imwj/bos/realm/BOSRealm.java
+++ bos-service/src/main/java/com/imwj/bos/realm/BOSRealm.java
@@ -6,6 +6,7 @@ import org.apache.shiro.authc.AuthenticationToken;
 import org.apache.shiro.authc.SimpleAuthenticationInfo;
 import org.apache.shiro.authc.UsernamePasswordToken;
 import org.apache.shiro.authz.AuthorizationInfo;
+import org.apache.shiro.authz.SimpleAuthorizationInfo;
 import org.apache.shiro.realm.AuthorizingRealm;
 import org.apache.shiro.subject.PrincipalCollection;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -35,7 +36,11 @@ public class BOSRealm extends AuthorizingRealm{
 	
 	//授权方法
 	protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection arg0) {
-		return null;
+		SimpleAuthorizationInfo info = new SimpleAuthorizationInfo();
+		info.addStringPermission("staff-list");
+		
+		//TODO 后期需要修改为根据当前登录用户查询数据库，获取实际对应的权限
+		return info;
 	}
 
 }
